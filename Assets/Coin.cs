a39aec9a525938304f178e2b46c666d5608e15ea
Michael Peterman <michaelkpeterman@gmail.com>
2017-03-21 17:26:27 -0400

    Begin building coin collectable. Create an assign sorting layer to player

---
+++ Assets/Coin.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Coin : MonoBehaviour {
+
+	// Check for collisions with player
+	void OnTriggerEnter2D( Collider2D other ) {
+		if ( other.gameObject.tag == "Player" ) {
+			Destroy ( gameObject );
+
+			// TODO: add points to score
+		}
+	}
+
+}
