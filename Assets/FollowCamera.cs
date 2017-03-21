9b2645579443f0afd4b8c5f0a0773336107e3c9c
michaelkpeterman <michael@36creative.com>
2017-03-20 21:40:07 -0400

    Begin designing environment

---
+++ Assets/FollowCamera.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCamera : MonoBehaviour {
+
+	public Transform gameObjectToFollow;
+	private Vector3 cameraStartingPos;
+
+	// Use this for initialization
+	void Start () {
+		// Position the camera on start
+		cameraStartingPos = new Vector3( gameObjectToFollow.position.x, gameObjectToFollow.position.y + 4f, -10f );
+	}
+
+	// Update is called once per frame
+	void LateUpdate () {
+		// Move the camera on player movement
+		if ( gameObjectToFollow.position.x >= 0f ) {
+			transform.position = new Vector3( gameObjectToFollow.position.x, transform.position.y, -10f );
+		} else if ( gameObjectToFollow.position.x < 0f ) {
+			transform.position = new Vector3( 0f, transform.position.y, -10f );
+		}
+	}
+}
